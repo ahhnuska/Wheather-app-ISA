9092c180a2266805477e5b1b18009f1a5d1fa2ed
luckyklyist <puspaanupam@gmail.com>
2023-07-15 20:14:39 +0545

    weather app date added

--- Wheatherapp/try.js
+++ Wheatherapp/try.js
@@ -6,22 +6,28 @@ const deleteWeatherInfo = () => {
     main_Division.removeChild(main_Division.firstChild);
   }
 };
-
-const done = async (cityname="Southampton") => {
+function currentData() {
+  let currentDate = new Date();
+  let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  let currentDayAndDate = currentDate.toLocaleDateString(undefined, options);
+  document.getElementById('current-day-and-date').textContent = currentDayAndDate;
+}
+const done = async (cityname = "Southampton") => {
   console.log("city name ", cityname);
   deleteWeatherInfo(); // Delete previous weather information
   
   const doing = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&units=metric&appid=${Api}`);
   const data = await doing.json();
   console.log(data)
-  if(data.cod!=200){
+  if (data.cod != 200) {
     alert("error")
     return;
-}
+  }
+  
+  const built = document.createElement("div");
+  
 
-const built = document.createElement("div");
-
-built.innerHTML = `
+  built.innerHTML = `
 <div class="outside">
 <img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png" alt="Weather Icon">
 <div class="des" style="font-size:12px"><i>${data.weather[0].description}</i></div>
@@ -31,29 +37,25 @@ built.innerHTML = `
   <div class="weather-info"> Pressure: ${data.main.pressure}mBar</div>
   <div class="weather-info">Windspeed: ${data.wind.speed}m/s</div>
   <div class="weather-info">Humidity: ${data.main.humidity}%</div>
+  <div id="current-day-and-date"></div>
 <div>
 <div>
 `;
-  
 
-  main_Division.append(built);
+
+main_Division.append(built);
+currentData()
   console.log(main_Division);
   console.log(data);
 };
-function currentData() {
-  let currentDate = new Date();
-  let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-  let currentDayAndDate = currentDate.toLocaleDateString(undefined, options);
-  document.getElementById('current-day-and-date').textContent = currentDayAndDate;
-}
-currentData()
+
 document.querySelector("#btnsearch").addEventListener("click", () => done(document.querySelector("#searchBar").value));
 
 document.querySelector("#searchBar").addEventListener("keydown", (e) => {
-    if (e.key==="Enter"){
-        done(document.querySelector("#searchBar").value);
+  if (e.key === "Enter") {
+    done(document.querySelector("#searchBar").value);
 
-    }
+  }
 
 });
 done()
